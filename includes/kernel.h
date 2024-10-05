1ffbd47ba953dabdd1f3999e863d7e2c102d4bbe
C-Chafik <qatar75020@gmail.com>
2024-10-05 20:48:50 +0200

    [+] forgot ';' when multi-line on DEFINE

--- includes/kernel.h
+++ includes/kernel.h
@@ -8,10 +8,10 @@ void kernel_colored_log(unsigned char color, char *format, ...);
 void kernel_panic(const char *function, const char *file, int line, char *format, ...);
 
 #define DUMP_STACK() \
-	print_kernel_stack(STACK_BASE, 200)
+	print_kernel_stack(STACK_BASE, 200);
 
 #define KERNEL_PANIC(format, ...) \
-	DUMP_STACK() \
-	kernel_panic(__FUNCTION__, __FILE__, __LINE__, format, ##__VA_ARGS__)
+	DUMP_STACK(); \
+	kernel_panic(__FUNCTION__, __FILE__, __LINE__, format, ##__VA_ARGS__);
 
 #endif
